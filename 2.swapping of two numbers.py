b0fbcec9a72026fed5a4b95d809368f41e01762d
Naveen <106677907+naveensc052@users.noreply.github.com>
2022-09-30 13:02:49 +0530

    Add files via upload

---
+++ 2.swapping of two numbers.py
@@ -0,0 +1,6 @@
+a=input("enter the number:")     #taking a number from user
+b=input("enter the number:")     #taking a number from user
+c=a                              #next three line manipulation
+a=b
+b=c
+print("a=",a,"b=",b)             #printing the final values of a and b that are swapped
